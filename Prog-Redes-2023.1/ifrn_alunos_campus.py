5b74bfc1244c12753624b07df6f734c65248835f
kakanetwork <Kalvimklain@gmail.com>
2023-04-08 18:42:27 -0300

    Atualizado

--- Prog-Redes-2023.1/ifrn_alunos_campus.py
+++ Prog-Redes-2023.1/ifrn_alunos_campus.py
@@ -1,5 +1,7 @@
 import requests, sys
 
+# Finalizado: 07/04/2023 
+
 # Url do site que iremos realizar o REQUEST
 url = 'https://dados.ifrn.edu.br/dataset/d5adda48-f65b-4ef8-9996-1ee2c445e7c0/resource/00efe66e-3615-4d87-8706-f68d52d801d7/download/dados_extraidos_recursos_alunos-da-instituicao.json'
 
