13ab55c3c2f53acb57cb2dcfe6a39deecda4ec9f
kakanetwork <Kalvimklain@gmail.com>
2023-04-15 23:41:58 -0300

    Atualizado

--- Prog-Redes-2023.1/Eleições.py
+++ Prog-Redes-2023.1/Eleições.py
@@ -35,7 +35,7 @@ import requests, os
 
 
 
-ano_eleição = int(input('Informe o ano de eleição desejado: '))
+'''ano_eleição = int(input('Informe o ano de eleição desejado: '))
 
 print('\nNúmeros dos respectivos cargos:')
 print("1 - Presidente\n3 - Governador\n5 - Senador\n6 - Deputado Federal\n7 - Desputado Estadual\n8 - Deputado Distrital\n11 - Prefeito\n13 - Vereador")
@@ -46,20 +46,26 @@ estado = str(input('\nInforme o estado desejado: ')).lower()
 print("\nNúmeros dos ID's das eleições:")
 print("544 - Eleição Federal\n546 - Eleição Estadual\n548 - Eleição de Conselheiro Distrital (Pernambuco)\n426 - Eleições Municipais\n438 - Eleição Suplementar Senador/MT")
 id = int(input('Informe o ID da eleição desejado: '))
+'''
+id = 544
+ano_eleição = 2022
+estado = 'br'
+cargo = 1
 
 url = f'https://resultados.tse.jus.br/oficial/ele{ano_eleição}/{id}/dados-simplificados/{estado}/{estado}-c{str(cargo).zfill(4)}-e000{id}-r.json'
-print(url)
 dados_gerais = requests.get(url).json()
 
-filtro = tuple(map(lambda c: (c['sqcand'], c['nm'], c['cc'], c['vap'], c['pvap']), dados_gerais['cand']))
+
+filtro = tuple(map(lambda c: (c['n'], c['nm'], c['cc'], c['vap'], c['pvap']), dados_gerais['cand']))
 filtro_organizado = sorted(filtro, key=lambda a: int(a[3]), reverse=True)
 numero_cand = dict()
 dir = os.path.dirname(os.path.abspath(__file__)) 
 file_dir = os.path.join(dir, 'resultados.txt')
-
 with open(file_dir, 'w', encoding='utf-8') as arq:
     arq.write('numero, nome, partido, quantidade_votos, percentual_votos\n')
     for x in filtro_organizado:
-        dados_cand = {'nome': x[1], 'partido': x[2], 'votos': x[3], 'percentual': x[4]} # Pesquisar: dict compreenshion 
-        numero_cand[x[0]] = dados_cand # Não entendi para que esse dicionário
-        arq.write(f'{(int(x[0]), x[1], x[2], int(x[3]), float(x[4].replace("," , ".")))}\n')
+        dados_cand = {'nome': x[1], 'partido': x[2], 'votos': x[3], 'percentual': x[4]}
+        numero_cand[x[0]] = dados_cand 
+        arq.write(f'{int(x[0])}, {x[1]}, {x[2]}, {int(x[3])}, {float(x[4].replace("," , "."))}\n')
+
+print(numero_cand)
