011888cbe3fef1da4f675765ad88e8f54f247ef0
kakanetwork <Kalvimklain@gmail.com>
2023-04-16 00:00:44 -0300

    Atualizado

--- Prog-Redes-2023.1/Eleições.py
+++ Prog-Redes-2023.1/Eleições.py
@@ -1,71 +1,54 @@
-'''
-    O TSE divulga na sua página oficial um webservice que fornece os dados 
-    das apurações das eleições realizadas no Brasil.
-
-    O fragmento de código a seguir monta um dicionário (dados_retorno) com o 
-    resultado das eleições do ano de 2022 no 1º turno para Presidente.
-
-    Com base na documentação da API contida na URL 
-    https://www.tse.jus.br/eleicoes/eleicoes-2022/interessados-na-divulgacao-de-resultados-2022
-
-    0001 Presidente Majoritário
-    0003 Governador Majoritário
-    0005 Senador Majoritário
-    0011 Prefeito Majoritário
-    0006 Deputado Federal Proporcional
-    0007 Deputado Estadual Proporcional
-    0008 Deputado Distrital Proporcional
-    0013 Vereador Proporcional
-    # Solicitar o ano,  solicitar a sigla do estado (ou br), cargo eletivo, id da eleição (ex: 544, 546)
-
-
-    Pede-se que seja desenvolvido um programa que solicite ao usuário o 
-    ano da eleição, tipo de eleição (estadual, nacional) e o cargo eletivo 
-    e o programa  deverá montar um dicionário {k:v} no seguinte formato:
-    {
-        num_candidato: { 'nome ': nome_candidato, 'partido': nome_partido, 
-                         'votos': quantidade_votos, 
-                         'percentual': percentual_votos},
-        num_candidato: { 'nome ': nome_candidato, 'partido': nome_partido, 
-                         'votos': quantidade_votos, 
-                         'percentual': percentual_votos},
-        ...'''
-
 import requests, os
 
 
+# Input de algumas informações para requisição
+ano_eleição = int(input('Informe o ano de eleição desejado: '))
 
-'''ano_eleição = int(input('Informe o ano de eleição desejado: '))
-
+# Exibindo opções de cargo para o usuario
 print('\nNúmeros dos respectivos cargos:')
 print("1 - Presidente\n3 - Governador\n5 - Senador\n6 - Deputado Federal\n7 - Desputado Estadual\n8 - Deputado Distrital\n11 - Prefeito\n13 - Vereador")
 cargo = int(input('Informe o Número do Cargo desejado: '))
+estado = str(input('\nInforme a sigla do estado desejado: ')).lower()
 
-estado = str(input('\nInforme o estado desejado: ')).lower()
-
+# Exibindo opções de ID's para usuario
 print("\nNúmeros dos ID's das eleições:")
 print("544 - Eleição Federal\n546 - Eleição Estadual\n548 - Eleição de Conselheiro Distrital (Pernambuco)\n426 - Eleições Municipais\n438 - Eleição Suplementar Senador/MT")
 id = int(input('Informe o ID da eleição desejado: '))
-'''
-id = 544
-ano_eleição = 2022
-estado = 'br'
-cargo = 1
 
+# Criando URL com base nas informações 
+# obs: realizo o zfill(4) no cargo, pois o usuario se informar valor 0003, o int desconsidera os zeros a esquerda, e transforma em 3
+# para evitar poderia ter colocado os 000 em string na URL, ou utilizar o ZFILL para completar em até 4 números, com o valor ZERO (foi oque escolhi)
 url = f'https://resultados.tse.jus.br/oficial/ele{ano_eleição}/{id}/dados-simplificados/{estado}/{estado}-c{str(cargo).zfill(4)}-e000{id}-r.json'
 dados_gerais = requests.get(url).json()
 
-
+# Realizando um filtro (map+lambda) para retirar da requisição URL, apenas os dados que quero, dentro de dados_gerais['cand'] que é a chave necessária 
 filtro = tuple(map(lambda c: (c['n'], c['nm'], c['cc'], c['vap'], c['pvap']), dados_gerais['cand']))
+
+# organizando meu filtro por ordem decrescente de acordo com a quantidade de votos de cada candidato (usando o lambda para verificar a quantidade de voto na a[3])
+# Reverse = True para ativar o modo decrescente 
 filtro_organizado = sorted(filtro, key=lambda a: int(a[3]), reverse=True)
+
+# definindo um dict
 numero_cand = dict()
+
+# pegando pasta atual + nome do arquivo a ser criado e concatenando eles
 dir = os.path.dirname(os.path.abspath(__file__)) 
 file_dir = os.path.join(dir, 'resultados.txt')
-with open(file_dir, 'w', encoding='utf-8') as arq:
-    arq.write('numero, nome, partido, quantidade_votos, percentual_votos\n')
-    for x in filtro_organizado:
-        dados_cand = {'nome': x[1], 'partido': x[2], 'votos': x[3], 'percentual': x[4]}
-        numero_cand[x[0]] = dados_cand 
-        arq.write(f'{int(x[0])}, {x[1]}, {x[2]}, {int(x[3])}, {float(x[4].replace("," , "."))}\n')
 
-print(numero_cand)
+# realizando abertura/criação do arquivo no modo Write
+with open(file_dir, 'w', encoding='utf-8') as arq:
+
+    # Adicionando o cabeçário do arquivo conforme pedido
+    arq.write('numero, nome, partido, quantidade_votos, percentual_votos\n')
+
+    # neste for, realizo várias coisas ao mesmo tempo... mas ele faz o X dentro
+    for x in filtro_organizado:
+
+        # aqui criei o dict com os dados de cada candidato por vez, pegando cada item pedido 
+        dados_cand = {'nome': x[1], 'partido': x[2], 'votos': x[3], 'percentual': x[4]}
+        # com o dict anterior criado, atribui cada dict de cada candidato, ao seu respectivo número como uma KEY, criando uma nova dict com a key = num candidato, e value = o dict anterior
+        numero_cand[x[0]] = dados_cand 
+        # finalmente termino com a criação do arquivo como pedido com algumas observações
+        # utilizo INT no X[0],[3] e float x[4] para deixar mais organizado, e no x[4] também substituo a Virgula pelo Ponto
+        # Dúvida: não precisei dos dicts anteriores, para fazer esse arquivo, porque precisaria deles?
+        arq.write(f'{int(x[0])}, {x[1]}, {x[2]}, {int(x[3])}, {float(x[4].replace("," , "."))}\n')
