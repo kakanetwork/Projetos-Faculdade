cbf5d433cbae03255aced6f922fec364148a067c
kakanetwork <Kalvimklain@gmail.com>
2023-04-29 11:09:17 -0300

    Atualizado

--- Projetos-Autorais/teste.py
+++ Projetos-Autorais/teste.py
@@ -1,55 +1,76 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import messagebox
+import os
+
+class FolderTemplate:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Folder Template")
+        
+        self.template_frame = tk.Frame(self.master)
+        self.template_frame.pack(side=tk.TOP, padx=10, pady=10)
+        
+        self.root_label = tk.Label(self.template_frame, text="Root Folder:")
+        self.root_label.grid(row=0, column=0, sticky="W")
+        
+        self.root_entry = tk.Entry(self.template_frame, width=50)
+        self.root_entry.grid(row=0, column=1, pady=5)
+        
+        self.template_label = tk.Label(self.template_frame, text="Folder Template:")
+        self.template_label.grid(row=1, column=0, sticky="W")
+        
+        self.template_text = tk.Text(self.template_frame, width=50, height=10)
+        self.template_text.grid(row=1, column=1, pady=5)
+        
+        self.add_button = tk.Button(self.template_frame, text="Add Folder", command=self.add_folder)
+        self.add_button.grid(row=2, column=0, padx=5, pady=5)
+        
+        self.rename_button = tk.Button(self.template_frame, text="Rename Folder", command=self.rename_folder)
+        self.rename_button.grid(row=2, column=1, padx=5, pady=5)
+        
+        self.create_button = tk.Button(self.template_frame, text="Create Folders", command=self.create_folders)
+        self.create_button.grid(row=3, column=1, pady=10)
+        
+        self.folder_list = []
+        self.folder_count = 1
+        
+    def add_folder(self):
+        self.folder_list.append("Folder " + str(self.folder_count))
+        self.folder_count += 1
+        self.template_text.insert(tk.END, self.folder_list[-1] + "\n")
+        
+    def rename_folder(self):
+        selected_folder = self.template_text.get("sel.first", "sel.last")
+        if selected_folder:
+            new_folder_name = simpledialog.askstring("Rename Folder", "Enter a new name for the folder", parent=self.master)
+            if new_folder_name:
+                self.template_text.delete(selected_folder)
+                self.template_text.insert(selected_folder, new_folder_name)
+        else:
+            messagebox.showwarning("No folder selected", "Please select a folder to rename.")
+            
+    def create_folders(self):
+        root_folder = self.root_entry.get()
+        if not root_folder:
+            messagebox.showwarning("No root folder specified", "Please specify a root folder.")
+            return
+        
+        folder_template = self.template_text.get("1.0", tk.END).strip()
+        if not folder_template:
+            messagebox.showwarning("Empty folder template", "Please specify a folder template.")
+            return
+        
+        folder_list = folder_template.split("\n")
+        
+        for folder in folder_list:
+            folder_path = os.path.join(root_folder, folder)
+            try:
+                os.makedirs(folder_path)
+            except FileExistsError:
+                pass
+            
+        messagebox.showinfo("Folders created", "Folders created successfully.")
 
 root = tk.Tk()
-root.title("Criar Árvore de Pastas")
-main_frame = ttk.Frame(root)
-main_frame.pack(fill=tk.BOTH, expand=True)
-
-label_frame = ttk.LabelFrame(main_frame, text="Criar Template de Árvore de Pastas")
-label_frame.pack(fill=tk.BOTH, expand=True, padx=10, pady=10)
-
-label = ttk.Label(label_frame, text="Digite o nome da pasta raiz:")
-label.pack(padx=10, pady=10)
-
-entry = ttk.Entry(label_frame)
-entry.pack(padx=10, pady=10)
-
-button_frame = ttk.Frame(label_frame)
-button_frame.pack(fill=tk.BOTH, expand=True, padx=10, pady=10)
-
-add_button = ttk.Button(button_frame, text="Adicionar Pasta")
-add_button.pack(side=tk.LEFT, padx=10)
-
-remove_button = ttk.Button(button_frame, text="Remover Pasta")
-remove_button.pack(side=tk.LEFT, padx=10)
-
-rename_button = ttk.Button(button_frame, text="Renomear Pasta")
-rename_button.pack(side=tk.LEFT, padx=10)
-
-treeview_frame = ttk.Frame(label_frame)
-treeview_frame.pack(fill=tk.BOTH, expand=True, padx=10, pady=10)
-
-treeview = ttk.Treeview(treeview_frame)
-treeview.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
-def add_folder():
-    selected_item = treeview.focus()
-    if not selected_item:
-        parent_item = ""
-    else:
-        parent_item = selected_item
-    treeview.insert(parent_item, "end", text="Nova Pasta")
-
-def remove_folder():
-    selected_item = treeview.focus()
-    treeview.delete(selected_item)
-
-def rename_folder():
-    selected_item = treeview.focus()
-    treeview.item(selected_item, open=True)
-    treeview.edit(selected_item)
-add_button.config(command=add_folder)
-remove_button.config(command=remove_folder)
-rename_button.config(command=rename_folder)
-
+app = FolderTemplate(root)
 root.mainloop()
