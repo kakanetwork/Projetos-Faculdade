421c665d333e36957e3c21b804865b58bbc1798c
kakanetwork <Kalvimklain@gmail.com>
2023-04-29 11:54:34 -0300

    Atualizado

--- Projetos-Autorais/teste.py
+++ Projetos-Autorais/teste.py
@@ -1,76 +1,64 @@
-import tkinter as tk
-from tkinter import messagebox
 import os
+import tkinter as tk
+from tkinter import filedialog
 
-class FolderTemplate:
+class App:
     def __init__(self, master):
         self.master = master
-        self.master.title("Folder Template")
-        
-        self.template_frame = tk.Frame(self.master)
-        self.template_frame.pack(side=tk.TOP, padx=10, pady=10)
-        
-        self.root_label = tk.Label(self.template_frame, text="Root Folder:")
-        self.root_label.grid(row=0, column=0, sticky="W")
-        
-        self.root_entry = tk.Entry(self.template_frame, width=50)
-        self.root_entry.grid(row=0, column=1, pady=5)
-        
-        self.template_label = tk.Label(self.template_frame, text="Folder Template:")
-        self.template_label.grid(row=1, column=0, sticky="W")
-        
-        self.template_text = tk.Text(self.template_frame, width=50, height=10)
-        self.template_text.grid(row=1, column=1, pady=5)
-        
-        self.add_button = tk.Button(self.template_frame, text="Add Folder", command=self.add_folder)
-        self.add_button.grid(row=2, column=0, padx=5, pady=5)
-        
-        self.rename_button = tk.Button(self.template_frame, text="Rename Folder", command=self.rename_folder)
-        self.rename_button.grid(row=2, column=1, padx=5, pady=5)
-        
-        self.create_button = tk.Button(self.template_frame, text="Create Folders", command=self.create_folders)
-        self.create_button.grid(row=3, column=1, pady=10)
-        
-        self.folder_list = []
-        self.folder_count = 1
-        
+        master.title("Criador de pastas")
+
+        self.folders = []
+
+        # label e botão para selecionar a pasta raiz
+        self.root_dir_label = tk.Label(master, text="Pasta raiz:")
+        self.root_dir_label.pack(side=tk.LEFT, padx=(10, 0), pady=10)
+        self.root_dir_var = tk.StringVar()
+        self.root_dir_entry = tk.Entry(master, textvariable=self.root_dir_var, width=40)
+        self.root_dir_entry.pack(side=tk.LEFT, padx=(0, 10), pady=10)
+        self.select_root_dir_button = tk.Button(master, text="Selecionar", command=self.select_root_dir)
+        self.select_root_dir_button.pack(side=tk.LEFT, pady=10)
+
+        # label e botão para adicionar nova pasta
+        self.folder_name_label = tk.Label(master, text="Nome da pasta:")
+        self.folder_name_label.pack(padx=10)
+        self.folder_name_var = tk.StringVar()
+        self.folder_name_entry = tk.Entry(master, textvariable=self.folder_name_var, width=40)
+        self.folder_name_entry.pack(padx=10)
+        self.add_folder_button = tk.Button(master, text="Adicionar pasta", command=self.add_folder)
+        self.add_folder_button.pack(pady=10)
+
+        # label e listbox para mostrar as pastas criadas
+        self.folders_label = tk.Label(master, text="Pastas criadas:")
+        self.folders_label.pack(padx=10)
+        self.folders_listbox = tk.Listbox(master, width=50)
+        self.folders_listbox.pack(padx=10)
+
+        # botão para criar as pastas na pasta raiz
+        self.create_folders_button = tk.Button(master, text="Criar pastas", command=self.create_folders)
+        self.create_folders_button.pack(pady=10)
+
+    def select_root_dir(self):
+        # abre uma janela para selecionar a pasta raiz
+        root_dir = filedialog.askdirectory()
+        self.root_dir_var.set(root_dir)
+
     def add_folder(self):
-        self.folder_list.append("Folder " + str(self.folder_count))
-        self.folder_count += 1
-        self.template_text.insert(tk.END, self.folder_list[-1] + "\n")
-        
-    def rename_folder(self):
-        selected_folder = self.template_text.get("sel.first", "sel.last")
-        if selected_folder:
-            new_folder_name = simpledialog.askstring("Rename Folder", "Enter a new name for the folder", parent=self.master)
-            if new_folder_name:
-                self.template_text.delete(selected_folder)
-                self.template_text.insert(selected_folder, new_folder_name)
-        else:
-            messagebox.showwarning("No folder selected", "Please select a folder to rename.")
-            
+        # adiciona uma nova pasta à lista de pastas criadas pelo usuário
+        folder_name = self.folder_name_var.get()
+        self.folders.append(folder_name)
+        self.folders_listbox.insert(tk.END, folder_name)
+
     def create_folders(self):
-        root_folder = self.root_entry.get()
-        if not root_folder:
-            messagebox.showwarning("No root folder specified", "Please specify a root folder.")
-            return
-        
-        folder_template = self.template_text.get("1.0", tk.END).strip()
-        if not folder_template:
-            messagebox.showwarning("Empty folder template", "Please specify a folder template.")
-            return
-        
-        folder_list = folder_template.split("\n")
-        
-        for folder in folder_list:
-            folder_path = os.path.join(root_folder, folder)
-            try:
-                os.makedirs(folder_path)
-            except FileExistsError:
-                pass
-            
-        messagebox.showinfo("Folders created", "Folders created successfully.")
+        # cria as pastas correspondentes na pasta raiz selecionada
+        root_dir = self.root_dir_var.get()
+        for folder_name in self.folders:
+            folder_path = os.path.join(root_dir, folder_name)
+            os.makedirs(folder_path)
+
+        # limpa a lista de pastas criadas pelo usuário
+        self.folders = []
+        self.folders_listbox.delete(0, tk.END)
 
 root = tk.Tk()
-app = FolderTemplate(root)
+app = App(root)
 root.mainloop()
