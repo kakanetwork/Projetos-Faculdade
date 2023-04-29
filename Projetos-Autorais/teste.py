5b8813d8017c8973a1bc2b2d0cdfed3140d89c02
kakanetwork <Kalvimklain@gmail.com>
2023-04-29 10:58:30 -0300

    Atualizado

--- Projetos-Autorais/teste.py
+++ Projetos-Autorais/teste.py
@@ -3,3 +3,32 @@ from tkinter import ttk
 
 root = tk.Tk()
 root.title("Criar Árvore de Pastas")
+main_frame = ttk.Frame(root)
+main_frame.pack(fill=tk.BOTH, expand=True)
+
+label_frame = ttk.LabelFrame(main_frame, text="Criar Template de Árvore de Pastas")
+label_frame.pack(fill=tk.BOTH, expand=True, padx=10, pady=10)
+
+label = ttk.Label(label_frame, text="Digite o nome da pasta raiz:")
+label.pack(padx=10, pady=10)
+
+entry = ttk.Entry(label_frame)
+entry.pack(padx=10, pady=10)
+
+button_frame = ttk.Frame(label_frame)
+button_frame.pack(fill=tk.BOTH, expand=True, padx=10, pady=10)
+
+add_button = ttk.Button(button_frame, text="Adicionar Pasta")
+add_button.pack(side=tk.LEFT, padx=10)
+
+remove_button = ttk.Button(button_frame, text="Remover Pasta")
+remove_button.pack(side=tk.LEFT, padx=10)
+
+rename_button = ttk.Button(button_frame, text="Renomear Pasta")
+rename_button.pack(side=tk.LEFT, padx=10)
+
+treeview_frame = ttk.Frame(label_frame)
+treeview_frame.pack(fill=tk.BOTH, expand=True, padx=10, pady=10)
+
+treeview = ttk.Treeview(treeview_frame)
+treeview.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
