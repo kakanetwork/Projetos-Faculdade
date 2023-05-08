d35fef206b910befac218b10fc65f129327abcb5
kakanetwork <Kalvimklain@gmail.com>
2023-05-07 23:24:02 -0300

    Atualizado

--- Projetos-Autorais/clear-temp.py
+++ Projetos-Autorais/clear-temp.py
@@ -9,4 +9,7 @@ lista = ["%userprofile%\AppData\Local\Temp", "%LOCALAPPDATA%\Microsoft\Windows\I
 lista_teste = ["C:\\Users\\USUARIO\OneDrive\Documents\s", "C:\\Users\\USUARIO\OneDrive\Documents\\a"]
 
 for x in lista_teste:
+    nomes = tuple(os.listdir(x))
+    for a in nomes:
+        os.remove('x\\a')
     print(f"apagando:... {os.listdir(x)}\n")
