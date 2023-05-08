018b5a32fa86ecb3c044619e7caa9aa7707755d4
kakanetwork <Kalvimklain@gmail.com>
2023-05-07 23:29:37 -0300

    Atualizado

--- Projetos-Autorais/clear-temp.py
+++ Projetos-Autorais/clear-temp.py
@@ -1,8 +1,5 @@
 import os, shutil
 
-
-
-lista = list()
 lista = ["%userprofile%\AppData\Local\Temp", "%LOCALAPPDATA%\Microsoft\Windows\INetCache", "%windir%\temp", 
 "C:\Windows\Prefetch", "C:\\Users\\USUARIO\Recent"]
 
@@ -11,6 +8,9 @@ lista_teste = ["C:\\Users\\USUARIO\OneDrive\Documents\s", "C:\\Users\\USUARIO\On
 for x in lista_teste:
     nomes = tuple(os.listdir(x))
     for a in nomes:
-        print(f'{x}\\{a}')
+        try:
+            os.remove(f'{x}\\{a}')
+        except IsADirectoryError:
+            shutil.rmtree(f'{x}\\{a}')
         print(f'Apagando...: {a}\n')
     #print(f"apagando:... {os.listdir(x)}\n")
