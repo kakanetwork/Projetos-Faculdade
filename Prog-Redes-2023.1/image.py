2d74e9882f5cb071bdae7df9eedcf1da52313270
kakanetwork <Kalvimklain@gmail.com>
2023-05-18 08:58:25 -0300

    Atualizado

---
+++ Prog-Redes-2023.1/image.py
@@ -0,0 +1,57 @@
+import socket
+
+
+#url = input('informa a url: ')
+url = str('https://www.nasa.gov/sites/default/files/thumbnails/image/nasa-logo-web-rgb.png')
+
+
+
+url_fragmentada = url.split('/')
+url_host = url_fragmentada[2]
+url_image = '/'.join(url_fragmentada[3:-1])
+arq_image = url_fragmentada[-1]
+extensão = arq_image.split('.')[-1]
+arq_txt = arq_image.replace(extensão, 'txt')
+
+print(url_fragmentada,url_host, url_image, arq_image, arq_txt, extensão)
+
+
+url_request = f'GET {url_image} HTTP/1.1\r\nHOST: {url_host}\r\n\r\n' 
+host_port   = 80
+buffer_size = 1024
+
+sock_img = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock_img.connect((url_host, host_port))
+sock_img.sendall(url_request.encode())
+
+print('\nBaixando a imagem...')
+
+
+# Montado a variável que armazenará os dados de retorno
+data_ret = b''
+while True:
+    data = sock_img.recv(buffer_size)
+    if not data: break
+    data_ret += data
+
+sock_img.close()
+
+# Obtendo o tamanho da imagem
+img_size = -1
+tmp = data_ret.split('\r\n'.encode())
+for line in tmp:
+   if 'Content-Length:'.encode() in line:
+      img_size = int(line.split()[1])
+      break
+print(f'\nTamanho da Imagem: {img_size} bytes')
+
+# Separando o Cabeçalho dos Dados
+delimiter = '\r\n\r\n'.encode()
+position  = data_ret.find(delimiter)
+headers   = data_ret[:position]
+image     = data_ret[position+4:]
+
+# Salvando a imagem
+file_output = open('image.png', 'wb')
+file_output.write(image)
+file_output.close()
