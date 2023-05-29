ba26bbfe485bd52bdf5a4f4db312c5d997e31244
kakanetwork <Kalvimklain@gmail.com>
2023-05-29 12:44:49 -0300

    Atualizado

--- Git_push.py
+++ Git_push.py
@@ -4,21 +4,28 @@
 import subprocess, time
 
 print('='*100)
+print('\nNo modo automático o commit será realizado a cada 1min até você parar a execução... no modo único ele será realizado apenas uma vez...\n')
+print('='*100)
 
 modo = input('\nEscolha o Modo Automatico(A) ou modo Único(U): ').upper()
 
 commit_name = 'Atualizado'
 branch_origin = 'master'
+tempo_seg = 120
 
 if modo == 'A':
     while True:
         subprocess.run(['git', 'add', '.'])
         subprocess.run(['git', 'commit', '-m', commit_name])
         subprocess.call(["git", "push", "-u", "origin", branch_origin])
-        time.sleep(120) 
+        print('='*100)
+        time.sleep(tempo_seg) 
+        
 elif modo == 'U':
     subprocess.run(['git', 'add', '.'])
     subprocess.run(['git', 'commit', '-m', commit_name])
     subprocess.call(["git", "push", "-u", "origin", branch_origin])
+    print('='*100)
+
 else:
     print('Escolha um modo válido!')
