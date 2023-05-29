0ffd1d9bd36f5bcab0b6751338ed287fd5420087
kakanetwork <Kalvimklain@gmail.com>
2023-05-29 12:42:10 -0300

    Atualizado

--- Git_push.py
+++ Git_push.py
@@ -4,17 +4,21 @@
 import subprocess, time
 
 print('='*100)
+
 modo = input('\nEscolha o Modo Automatico(A) ou modo Único(U): ').upper()
+
+commit_name = 'Atualizado'
+branch_origin = 'master'
 
 if modo == 'A':
     while True:
         subprocess.run(['git', 'add', '.'])
-        subprocess.run(['git', 'commit', '-m', 'Atualizado'])
-        subprocess.call(["git", "push", "-u", "origin", "master"])
+        subprocess.run(['git', 'commit', '-m', commit_name])
+        subprocess.call(["git", "push", "-u", "origin", branch_origin])
         time.sleep(120) 
 elif modo == 'U':
     subprocess.run(['git', 'add', '.'])
-    subprocess.run(['git', 'commit', '-m', 'Atualizado'])
-    subprocess.call(["git", "push", "-u", "origin", "master"])
+    subprocess.run(['git', 'commit', '-m', commit_name])
+    subprocess.call(["git", "push", "-u", "origin", branch_origin])
 else:
     print('Escolha um modo válido!')
