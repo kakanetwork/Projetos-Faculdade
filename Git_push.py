c16f65d165453620346b88c23cbd2eb0aae6304a
kakanetwork <Kalvimklain@gmail.com>
2023-05-29 12:37:01 -0300

    Atualizado

--- Git_push.py
+++ Git_push.py
@@ -2,9 +2,19 @@
 # Código para realizar commit e push automatico no GITHUB
 
 import subprocess, time
-while True:
+
+print('='*100)
+modo = input('\nEscolha o Modo Automatico(A) ou modo Único(U): ')
+
+if modo == 'A':
+    while True:
+        subprocess.run(['git', 'add', '.'])
+        subprocess.run(['git', 'commit', '-m', 'Atualizado'])
+        subprocess.call(["git", "push", "-u", "origin", "master"])
+        time.sleep(120) 
+elif modo == 'U':
     subprocess.run(['git', 'add', '.'])
     subprocess.run(['git', 'commit', '-m', 'Atualizado'])
     subprocess.call(["git", "push", "-u", "origin", "master"])
-    time.sleep(120) 
-    
+else:
+    print('Escolha um modo válido!')
