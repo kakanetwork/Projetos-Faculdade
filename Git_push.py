19c25e152990c83f7498aa4ad4d332bb79e92bc2
kakanetwork <Kalvimklain@gmail.com>
2023-05-28 23:24:44 -0300

    Atualizado

--- Git_push.py
+++ Git_push.py
@@ -1,7 +1,10 @@
 
 # Código para realizar commit e push automatico no GITHUB
 
-import subprocess
-subprocess.run(['git', 'add', '.'])
-subprocess.run(['git', 'commit', '-m', 'Atualizado'])
-subprocess.call(["git", "push", "-u", "origin", "master"])
+import subprocess, time
+while True:
+    subprocess.run(['git', 'add', '.'])
+    subprocess.run(['git', 'commit', '-m', 'Atualizado'])
+    subprocess.call(["git", "push", "-u", "origin", "master"])
+    time.sleep(30)
+    
