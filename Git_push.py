192b1458f4d1251d57333ad0a735f53d7446c681
kakanetwork <Kalvimklain@gmail.com>
2023-05-31 01:22:33 -0300

    Atualizado

--- Git_push.py
+++ Git_push.py
@@ -9,32 +9,90 @@ else:
     os.system('clear')
 
 print('='*100)
-print('\nFUNCIONAMENTO DO CÓDIGO:\nNo modo automático o commit será realizado a cada 1min até você parar a execução... no modo único ele será realizado apenas uma vez...\n')
+ascii_art = r"""
+   _____ _ _   _           _                     _                        _   _             
+  / ____(_) | | |         | |         /\        | |                      | | (_)            
+ | |  __ _| |_| |__  _   _| |__      /  \  _   _| |_ ___  _ __ ___   __ _| |_ _  ___  _ __  
+ | | |_ | | __| '_ \| | | | '_ \    / /\ \| | | | __/ _ \| '_ ` _ \ / _` | __| |/ _ \| '_ \ 
+ | |__| | | |_| | | | |_| | |_) |  / ____ \ |_| | || (_) | | | | | | (_| | |_| | (_) | | | |
+  \_____|_|\__|_| |_|\__,_|_.__/  /_/    \_\__,_|\__\___/|_| |_| |_|\__,_|\__|_|\___/|_| |_|
+                                                                                            
+                                     by - kakanetwork
+ """                                                                                                                       
+print(ascii_art)
+
 print('='*100)
 
-modo = ''
-while modo != 'A' and modo != 'U':
-    modo = input('\nEscolha o Modo: Automático(A) Único(U) ou Sair(S): ').upper()
-    if modo == 'S':
+def Git_Pull ():
+    subprocess.run(['git', 'pull'])
 
-        print('\n'+'='*100)
-        print('\nCódigo encerrado com sucesso!\n')
-        break
+def Git_Push ():
+    print('='*100)
+    modo = ''
+    while modo not in ['A', 'U', 'S', '?']:
+        modo = str(input('\nModos:\nA - Automatico\nU - Único\nS - Sair\n? - Ajuda\n\nQual você deseja? ').upper())
+        if modo not in ['A', 'U', 'S', '?']:
+            print('Tente Novamente... informe corretamente!')
+    commit_name = 'Atualizado'
+    branch_origin = 'master'
+    tempo_seg = 120
+    if modo == 'A':
+        while True:
+            subprocess.run(['git', 'add', '.'])
+            subprocess.run(['git', 'commit', '-m', commit_name])
+            subprocess.call(["git", "push", "-u", "origin", branch_origin])
+            print('='*100)
+            time.sleep(tempo_seg) 
 
-commit_name = 'Atualizado'
-branch_origin = 'master'
-tempo_seg = 120
-
-if modo == 'A':
-    while True:
+    elif modo == 'U':
         subprocess.run(['git', 'add', '.'])
         subprocess.run(['git', 'commit', '-m', commit_name])
         subprocess.call(["git", "push", "-u", "origin", branch_origin])
-        print('='*100)
-        time.sleep(tempo_seg) 
+        print('\n'+'='*100)
 
-elif modo == 'U':
-    subprocess.run(['git', 'add', '.'])
-    subprocess.run(['git', 'commit', '-m', commit_name])
-    subprocess.call(["git", "push", "-u", "origin", branch_origin])
-    print('\n'+'='*100)
+    elif modo == '?':
+        print('='*100)
+        print(f'\nExplicação das Variavéis:\n\nAutomático -> Será gerado um Commit a cada {tempo_seg}s até que você encerre o programa! (se desejar alterar os segundos, acesse a configuração)\n\
+Único -> Será gerado um Commit Único e o Programa será encerrado!\n\nCommit_name -> O nome do seu commit, pode ser genérico (se necessário alteração, vá nas configurações)!\n\
+Branch_Origin -> O nome do seu branch default no Github (se necessário alteração, vá nas configurações)!\n')
+        print('='*100)
+    elif modo == 'S':
+        print('\nPrograma Encerrado com Sucesso!\n')
+        exit()
+
+def Git_Connect ():
+    print('\ndesenvolvimento...')
+
+def Ajuda ():
+    print('='*100)
+    print(f'\nGit Push -> Realiza o Push do seus Commits, ou seja leva a atualização deles para o seu github\nGit Pull -> Realiza o Pull dos seus commits, \
+ou seja traz as atualizações do seu código no github para seu ambiente de programação\nGit Connect -> Vai realizar a conexão do seu github com o seu VSCODE \
+automaticamente (Recomendado realizar antes de qualquer Pull ou Push.)!\n')
+    print('='*100)
+
+def Sair ():
+    print('\n Programa Encerrado')
+    exit()
+
+opções = {
+    '1': Git_Push,
+    '2': Git_Pull,
+    '3': Git_Connect,
+    '4': Ajuda,
+    '5': Sair
+}   
+
+print('\nFerramentas Disponiveis:\n')
+
+for key, value in opções.items():
+    print(f'{key}: {value.__name__}')
+
+ferramenta = ''
+
+while ferramenta not in opções:
+    ferramenta = input('\nQual Ferramenta deseja utilizar? ')
+    if ferramenta not in opções:
+        print('Tente Novamente... informe corretamente!')
+print(f'Você escolhou a ferramenta: {opções[ferramenta].__name__}\n')
+opções[ferramenta]()
+
