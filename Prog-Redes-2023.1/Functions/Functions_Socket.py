75342ffb7c49972ab52beba09384f2a967636aae
kakanetwork <Kalvimklain@gmail.com>
2023-06-01 01:22:37 -0300

    Atualizado

--- Prog-Redes-2023.1/Functions/Functions_Socket.py
+++ Prog-Redes-2023.1/Functions/Functions_Socket.py
@@ -2,37 +2,27 @@ import socket, ssl, sys
 
 # verifica se a url é HTTP ou HTTPS
 def socket_https(url_image, url_host, buffer_size):
-
     # define a requisição 
     url_request = f'GET {url_image} HTTP/1.1\r\nHOST: {url_host}\r\n\r\n' 
-
     # criação do contexto SSL para conexão HTTPS
     context = ssl.create_default_context()
-
     # desativa a verificação do nome do host durante a autenticação SSL.
     context.check_hostname = False
-
     # o certificado do servidor não será verificado
     context.verify_mode = ssl.CERT_NONE
-
     # criação do socket/ conexão com o server (IPV4/TCP)
     socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
     # Envolve o socket criado anteriormente em uma conexão segura (wrap_socket)
     socket_conexão = context.wrap_socket(socket, server_hostname=url_host)
-
     try:
         # estabelece a conexão
         socket_conexão.connect((url_host, 443))
-
         # enviando requisição pedida acima
         socket_conexão.send(url_request.encode('utf-8'))
     except:
         print(f'Erro...{sys.exc_info()[0]}')
         exit()
-
     print('\nBaixando a imagem...\n')
-
     # recebendo a resposta 
     data_ret = b''
     try:
@@ -45,7 +35,6 @@ def socket_https(url_image, url_host, buffer_size):
     except:
         print(f'Erro...{sys.exc_info(0)}')  
         exit()  
-
     # fechando conexão
     socket_conexão.close()
     return data_ret
@@ -53,22 +42,17 @@ def socket_https(url_image, url_host, buffer_size):
 def socket_http (url_image, url_host, buffer_size):
     # define a requisição 
     url_request = f'GET {url_image} HTTP/1.1\r\nHOST: {url_host}\r\n\r\n' 
-
     # criando conexão IPV4(AF.INET) e TCP(SOCK_STREAM)
     socket_conexão = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
     try:
         # Realizando a conexão
         socket_conexão.connect((url_host, 80))
-
         # enviando requisição pedida acima
         socket_conexão.sendall(url_request.encode('utf-8'))
     except:
         print(f'Erro...{sys.exc_info()[0]}')    
         exit()
-        
     print('\nBaixando a imagem...')
-
     #Recebendo os dados
     data_ret = b''
     try:
@@ -85,7 +69,6 @@ def socket_http (url_image, url_host, buffer_size):
     except:
         print(f'Erro...{sys.exc_info()[0]}')
         exit()
-
     # fechando conexão
     socket_conexão.close()
     return data_ret
