286a1221418fbc6a257fd31775dcdc62aa7e8076
kakanetwork <Kalvimklain@gmail.com>
2023-06-18 02:04:11 -0300

    Atualização

--- Shell Scripts - Estudos/Calculadora_V2.sh
+++ Shell Scripts - Estudos/Calculadora_V2.sh
@@ -4,17 +4,24 @@
 user=$(whoami)
 
 echo "\nBem vindo $user - Calculadora Simples\n"
-echo 'informe o tipo de equação 1(+), 2(-), 3(*) ou 4(/): '
+echo -n 'informe o tipo de equação 1(+), 2(-), 3(*) ou 4(/): '
 read option
 
-if [ $option -eq 1 ];then
-    echo "oi"
-    
 echo -n 'Informe um valor inteiro para a equação: '
 read var1
 echo -n 'Informe um segundo valor inteiro para a equação: '
 read var2
-
 echo "\nO número digitado foi: $var1 e $var2"
-echo "\nA soma é igual: $((var1+var2))\nA Subtração é igual: $((var1-var2))\n\
-A Multiplicação é igual: $((var1*var2))\nA Divisão é igual: $((var1/var2))\n"
+
+if [ $option -eq 1 ];then
+    echo "A soma é igual: $((var1+var2))\n"
+elif [ $option -eq 2 ];then
+    echo "A Subtração é igual: $((var1-var2))\n"
+elif [ $option -eq 3];then
+    echo "A Multiplicação é igual: $((var1*var2))\n"
+elif [ $option -eq 4 ];then
+    echo "A Divisão é igual: $((var1/var2))\n"
+else 
+    echo "Informe um número correto!\n"
+fi
+
