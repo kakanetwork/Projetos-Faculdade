105e741dbf32f42ae0322793e112b7a2a058b0e6
kakanetwork <Kalvimklain@gmail.com>
2023-06-28 00:54:11 -0300

    Atualização

--- Prog-Redes-2023.1/Sockets - Client and Server (TCP)/tcp_server.py
+++ Prog-Redes-2023.1/Sockets - Client and Server (TCP)/tcp_server.py
@@ -1,21 +1,41 @@
 import socket, sys, os, time
 from socket_constants import *
-
 # Criação do socket (IPV4 / TCP)
 socket_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Vinculando ao host e porta 
 socket_tcp.bind((HOST_SERVER,SOCKET_PORT))
 
-print(f'\nSERVIDOR ATIVO: {socket_tcp.getsockname()}')
+# definindo quantas conexões ele ira atender
+socket_tcp.listen(1)
+
+# informações do servidor ativo (ip e porta)
+print(f'\nSERVIDOR ATIVO: {socket_tcp.getsockname()}\n')
+
+socket_conexão, ip_client = socket_tcp.accept()
+print(f'O cliente de ip: {ip_client[0]}\nNa porta: {ip_client[1]}\nFoi conectado com sucesso ao servidor!\n')   
+print('Esperando Recebimento de mensagens...\n')
+
+while True:
+    msg_client = socket_conexão.recv(BUFFER_SIZE)
+    msg_client = msg_client.decode(CODE_PAGE)
+    if msg_client.upper() == 'EXIT':
+        print(f'\nO cliente {ip_client} se desconectou do servidor...!\n')
+        break
+    else:
+        nome_arquivo = ATUAL_DIR + '\\img_server\\' + msg_client
+        print(f'Enviando o arquivo: {msg_client}\n')
+        size_arq = os.path.getsize(nome_arquivo)
+        msg_server = f'Size: {size_arq}'.encode(CODE_PAGE)
+        socket_conexão.send(msg_server)
+        with (nome_arquivo, 'rb') as arquivo:
+            while True:
+                dados_img = arquivo.read(BUFFER_SIZE)
+                if not data_retorno:
+                    break
+                
+    break
 '''
-# Criando o socket UDP
-socket_TCP_IPV4 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Vincular o socket a tupla (host, port)
-socket_TCP_IPV4.bind((HOST_SERVER, SOCKET_PORT)) 
-
-print(f'\nSERVIDOR ATIVO: {socket_TCP_IPV4.getsockname()}')
 print('\nRecebendo Mensagens...\n\n')
 
 try:
