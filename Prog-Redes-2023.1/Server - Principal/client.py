96a5711f8fd0c1ccefdb4bd816255b9b66fe69d2
kakanetwork <Kalvimklain@gmail.com>
2023-07-09 00:06:50 -0300

    Atualização

--- Prog-Redes-2023.1/Server - Principal/client.py
+++ Prog-Redes-2023.1/Server - Principal/client.py
@@ -1,15 +1,41 @@
 import socket, threading, os, sys
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '\\Functions and Variables')
 from variables import *
-from functions_client import *
+#from functions_client import *
 from functions_others import PRINT_DIV
+
+def closeSocket():
+    try:
+        sock.close()
+    except:
+        None
+
+def servInteraction():
+    msg = b' '
+    while msg != b'':
+        try:
+            msg = sock.recv(512)
+            print ("\n"+msg.decode('utf-8')+"\n"+PROMPT)
+        except:
+            msg = b''
+    closeSocket()
+
+def userInteraction():
+    msg = ''
+    while msg != '/q':
+        try:
+            msg = input(PROMPT)
+            if msg != '': sock.send(msg.encode('utf-8'))
+        except:
+            msg = '/q'
+    closeSocket()
 
 
 try:
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((SERVER_CLIENT, PORT))
 
-    PRINT_DIV ("Conectado a: ", (SERVER_CLIENT, PORT))
+    PRINT_DIV(f"Você se conectou com sucesso ao Server de IP: {SERVER} | Na Porta: {PORT}")
     tServer = threading.Thread(target=servInteraction)
     tUser = threading.Thread(target=userInteraction)
 
