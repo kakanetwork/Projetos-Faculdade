d74fee4ed1da4feedfda30128593aab6c72cb01a
kakanetwork <Kalvimklain@gmail.com>
2023-07-09 01:27:26 -0300

    Atualização

--- Prog-Redes-2023.1/Server - Principal/Functions and Variables/functions_server.py
+++ Prog-Redes-2023.1/Server - Principal/Functions and Variables/functions_server.py
@@ -8,6 +8,7 @@ def LIST_CLIENTS(clients_connected):
     for chave, valor in clients_connected.items():
         ip = valor[0]
         msg = f"IP: {ip}\nPORT: {chave}"
+        print('chegooo')
 
 def OPTIONS():
     opções_descritivas = {
@@ -15,24 +16,27 @@ def OPTIONS():
         }
     
 def CLIENT_INTERACTION(sock_client, info_client, clients_connected):
+    opções = {'/l': LIST_CLIENTS(clients_connected)}
     msg = b'' # definindo uma mensagem binária
     while msg != b'/q': # o while 
         try:
-            opções = {'/l': LIST_CLIENTS(clients_connected)}
-            msg = sock_client.recv(512) 
-            msg = f"{addrSource} -> {msg.decode('utf-8')}"
-            #for msg in opções:
-            #    opções[msg]()
+            msg = sock_client.recv(512).decode('utf-8')
+            print(msg)
+            for msg in opções[keys]:
+                print('for')
+                if msg == opções:
+                    print('foi')
+                    opções[msg]()
+
             #broadCast (msg, info_client)
         except:
             msg = b'/q'
     del clients_connected[info_client[1]]
     sock_client.close()
-    return msg
 
 def broadCast(msg, addrSource):
     #msg = f"{addrSource} -> {msg.decode('utf-8')}"
-    PRINT_DIV(msg)
+    #PRINT_DIV(msg)
     for sockConn, addr in clients_connected:
         if addr != addrSource:
             sockConn.send(msg.encode('utf-8'))
