3ef9b805ce41af57b52b4f9eb23626ed24f7a5aa
kakanetwork <Kalvimklain@gmail.com>
2023-07-15 18:51:12 -0300

    Atualização

--- Prog-Redes-2023.1/Server - Principal/Functions and Variables/functions_bot.py
+++ Prog-Redes-2023.1/Server - Principal/Functions and Variables/functions_bot.py
@@ -17,16 +17,14 @@ def NOTIFICATION_BOT(msg_connected):
 # ============================================================================================================
 
 def LIST_CLIENTS_BOT(clients_connected):
-    try: 
-        resposta = {'chat_id':id_chat,'text':f'{clients_connected}'}
-        var = requests.post(url_req+'/sendMessage',data=resposta)
-        #msg_title = "\nOs Clientes conectados ao Servidor são:" # formatando mensagem 
-        '''num = 0
-        for chave, valor in clients_dict.items():  # faço um for para pegar cada cliente conectado e enviar 
+    try:
+        num = 0 
+        for chave, valor in clients_connected.items():
             ip = valor[0] # Armazenamento Temporário 
             num+=1 # formatação numeração cliente
-            msg_list = f"\nCLIENTE {num}\nIP: {ip}\nPORT: {chave}\n" # formatação listagem clientes (lembrando que chave=porta e valor[0]=ip) '''
-
+            msg_list = f"\nCLIENTE {num}\nIP: {ip}\nPORT: {chave}\n" # formatação listagem clientes (lembrando que chave=porta e valor[0]=ip) '''    
+            resposta = {'chat_id':id_chat,'text':f'{msg_list}'}
+            var = requests.post(url_req+'/sendMessage',data=resposta)
     except:
         print(f'\nErro no momento de Listar os Clientes Conectados...{sys.exc_info()[0]}')  
         exit() 
