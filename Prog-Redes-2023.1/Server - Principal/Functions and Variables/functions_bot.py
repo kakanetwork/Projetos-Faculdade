5e75da709511d80169c2411420d754b4e3dcfb02
kakanetwork <Kalvimklain@gmail.com>
2023-07-15 18:03:57 -0300

    Atualização

--- Prog-Redes-2023.1/Server - Principal/Functions and Variables/functions_bot.py
+++ Prog-Redes-2023.1/Server - Principal/Functions and Variables/functions_bot.py
@@ -2,14 +2,18 @@
 import requests
 from credentials import *
 
-
-
+def REQUEST_BOT():
+    try:
+        url_req = f'https://api.telegram.org/bot{API_key}'
+        return url_req
+    except:
+        print(f'\nErro no Request do Bot Telegram...{sys.exc_info()[0]}')  
 
 # ============================================================================================================
 
 def NOTIFICATION_BOT(msg):
     try:
-        url_req = f'https://api.telegram.org/bot{API_key}'
+        url_req = REQUEST_BOT
         resposta = {'chat_id':id_chat,'text':f'{msg}'}
         var = requests.post(url_req+'/sendMessage',data=resposta)
     except:
@@ -20,7 +24,7 @@ def NOTIFICATION_BOT(msg):
 def COMMAND_BOT():
     id_message = None
     while True:
-        url_req = f'https://api.telegram.org/bot{API_key}'
+        url_req = REQUEST_BOT
         chat = requests.get(url_req + '/getUpdates', params={'offset': id_message}).json().get('result', [])
         for message in chat:
             command = message.get('message', []).get('text', [])
@@ -34,12 +38,18 @@ def COMMAND_BOT():
 
 def LIST_CLIENTS_BOT(clients_dict):
     try: 
-        msg_title = "\nOs Clientes conectados ao Servidor são:" # formatando mensagem 
+        url_req = REQUEST_BOT
+        resposta = {'chat_id':id_chat,'text':f'{clientes_dict}'}
+        var = requests.post(url_req+'/sendMessage',data=resposta)
+
+
+        #msg_title = "\nOs Clientes conectados ao Servidor são:" # formatando mensagem 
         '''num = 0
         for chave, valor in clients_dict.items():  # faço um for para pegar cada cliente conectado e enviar 
             ip = valor[0] # Armazenamento Temporário 
             num+=1 # formatação numeração cliente
             msg_list = f"\nCLIENTE {num}\nIP: {ip}\nPORT: {chave}\n" # formatação listagem clientes (lembrando que chave=porta e valor[0]=ip) '''
+
     except:
         print(f'\nErro no momento de Listar os Clientes Conectados...{sys.exc_info()[0]}')  
         exit() 
