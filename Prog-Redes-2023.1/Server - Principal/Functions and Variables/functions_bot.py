92e9ddcaa79b2d984c37d3541a3b59ac9a49a0ea
kakanetwork <Kalvimklain@gmail.com>
2023-07-17 13:31:12 -0300

    Atualização

--- Prog-Redes-2023.1/Server - Principal/Functions and Variables/functions_bot.py
+++ Prog-Redes-2023.1/Server - Principal/Functions and Variables/functions_bot.py
@@ -30,11 +30,11 @@ def VERIFICATION_KEY_ID():
                 id_chat = verification_id[0]['message']['chat']['id'] # havendo mensagens, ele ira capturar o seu id para prosseguir
                 return id_chat # retornando o id 
             else:
-                raise SystemExit('Você não possui mensagens armazenadas no Bot, por favor envie qualquer mensagem!')
+                raise SystemExit
         else:
-            raise SystemExit(f'\nA chave: {API_key}\nInformada é inválida!\n')
+            raise SystemExit
     except:
-        print(f'\nErro na Verificação da API_KEY...{sys.exc_info()[0]}')  
+        print(f'\nErro na Verificação da API_KEY...{sys.exc_info()}')  
         exit()
    
 id_chat = VERIFICATION_KEY_ID()
