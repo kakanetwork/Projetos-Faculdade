1f9cf19a3efb71f3a3d8fcbb69ea0b5fe150b5b9
kakanetwork <Kalvimklain@gmail.com>
2023-07-18 16:19:19 -0300

    Atualização

--- Prog-Redes-2023.1/teste.py
+++ Prog-Redes-2023.1/teste.py
@@ -3,20 +3,20 @@ import os, subprocess
 dir_atual = os.path.dirname(os.path.abspath(__file__))  # pegando a pasta atual
 dir_arq =  os.path.abspath(__file__)
 dir_temp = dir_atual + "\\Server-Principal"
+'''
 
-pid = 58552
 proc = subprocess.check_output(['tasklist', '/NH', '/FI', f'PID eq {pid}'])
-print(proc)
-
+print(proc)'''
+pid = 58552
 processo = subprocess.run(['Powershell', 'Get-Process', '-Id', pid], capture_output=True, text=True)
 saida = processo.stdout.strip()
 print(saida)
 print(processo)
 
 
-pid_file = dir_atual + '\\pid.temp'
+'''pid_file = dir_atual + '\\pid.temp'
 
 with open(pid_file, 'r') as file:
     pid = int(file.readline().strip())
     print(type(pid))
-    print(pid)
+    print(pid)'''
