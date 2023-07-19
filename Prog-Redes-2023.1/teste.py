41eff060c9a272d892586a19a86067258f810a15
kakanetwork <Kalvimklain@gmail.com>
2023-07-19 11:40:02 -0300

    Atualização

--- Prog-Redes-2023.1/teste.py
+++ Prog-Redes-2023.1/teste.py
@@ -7,14 +7,11 @@ dir_temp = dir_atual + "\\Server-Principal"
 
 proc = subprocess.check_output(['tasklist', '/NH', '/FI', f'PID eq {pid}'])
 print(proc)'''
-pid = '5855aa2'
-processo = subprocess.run(['Powershell', 'Get-Process', '-Id', pid], capture_output=True, text=True).stdout.strip()
-if processo:
-    print('ao')
+pid = '54360'
+if subprocess.run(['Powershell', 'Get-Process', '-Id', pid], capture_output=True, text=True).stdout.strip() != '':
+    print('o1')
 else:
-    print('o')
-
-print(processo)
+    print('a')
 
 
 '''pid_file = dir_atual + '\\pid.temp'
